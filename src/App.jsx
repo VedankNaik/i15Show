5a8f5aad7e6a307dbb695173f02f65158c3cd56c
Vedank Naik <vedankn01@gmail.com>
2024-04-16 12:50:46 +0530

    Add working, footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import HowItWorks from "./components/HowItWorks";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
@@ -11,7 +13,9 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
-      <Features />      
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   );
 };
